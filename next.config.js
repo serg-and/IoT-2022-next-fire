3219cc9079696bdeba2cb0c229173f46ad61db78
serg-and <serge.andriessen@gmail.com>
2023-01-03 19:41:34 +0100

    load firestore from environment variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
+  }
 }
 
 export default nextConfig
--- utils/firebaseConfig.js
+++ utils/firebaseConfig.js
@@ -2,15 +2,19 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8np8HRa9dpQyegt6pN45Ws7aROPJjkFI",
-  authDomain: "crypto-tracker-cc922.firebaseapp.com",
-  databaseURL: "https://crypto-tracker-cc922-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "crypto-tracker-cc922",
-  storageBucket: "crypto-tracker-cc922.appspot.com",
-  messagingSenderId: "876150894888",
-  appId: "1:876150894888:web:164f5a6df9db6c63fa201d",
-  measurementId: "G-57VS3DXC49"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 }
 
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+
+console.log(firebaseConfig)
+console.log(app)
+console.log(db)
